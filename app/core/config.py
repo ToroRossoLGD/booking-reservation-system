4752556b601104e8a2d7861f444c9d9d4348a128
uros jovic <uros1005@gmail.com>
2026-06-17 15:01:57 +0200

    Add Celery Beat schedule for reservation expiration

--- app/core/config.py
+++ app/core/config.py
@@ -27,6 +27,7 @@ class Settings(BaseSettings):
 
     CELERY_BROKER_URL: str = "redis://localhost:6379/1"
     CELERY_RESULT_BACKEND: str = "redis://localhost:6379/2"
+    CELERY_EXPIRE_PENDING_INTERVAL_MINUTES: int = 5
 
     RESERVATION_EXPIRE_MINUTES: int = 15
     
